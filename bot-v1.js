e766d7bcf1fffd027e224040a013855af8c885f7
bushpeddler <189076781+bushpeddler@users.noreply.github.com>
2025-01-30 20:47:38 -0500

    crazy

--- bot-v1.js
+++
@@ -1,9 +0,0 @@
-const { Telegraf } = require('telegraf');
-require('dotenv').config();
-
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-
-bot.start((ctx) => ctx.reply("Welcome to Arcane Empires!"));
-bot.launch().then(() => {
-  console.log("Bot is running!");
-});
